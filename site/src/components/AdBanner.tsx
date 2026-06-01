51341255489d3daec7842875193bcf790981b6ef
zxwmpowd <mateusalves13alves@gmail.com>
2026-06-01 02:47:03 -0300

    Add files via upload

---
+++ site/src/components/AdBanner.tsx
@@ -0,0 +1,22 @@
+export default function AdBanner() {
+  return (
+    <div className="w-full my-8 flex justify-center items-center overflow-hidden">
+      <iframe
+        data-aa="2440274"
+        src="//acceptable.a-ads.com/2440274"
+        style={{
+          border: 0,
+          padding: 0,
+          width: '100%',
+          maxWidth: '728px',
+          height: '90px',
+          overflow: 'hidden',
+          backgroundColor: 'transparent',
+          display: 'block',
+          margin: 'auto',
+        }}
+        title="Advertisement"
+      />
+    </div>
+  );
+}
